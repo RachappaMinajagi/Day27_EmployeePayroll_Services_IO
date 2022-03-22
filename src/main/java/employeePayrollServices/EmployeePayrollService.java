b95b577ecd6250e16fdb4ce9c485f60ddc2ceda7
Rachappa-Minajagi <m.rachoti142@gmail.com>
2022-03-22 23:35:23 +0530

    Added:Create a watch service

--- src/main/java/employeePayrollServices/EmployeePayrollService.java
+++ src/main/java/employeePayrollServices/EmployeePayrollService.java
@@ -3,6 +3,14 @@ package employeePayrollServices;
 /**
  * UC1:- Create an Employee Payroll Service to Read and Write Employee 
  * Payroll to a Console - Create Employee Payroll Class of id, name and Salary
+ * UC2:- Write a program to demonstrate File Operations like
+         - Check File Exists
+         - Delete File and Check File Not Exist
+         - Create Directory
+         - Create Empty File
+         - List Files, Directories as well as Files with Extension
+    UC3:-Create a Watch Service to watch particular directory along with all Files and
+          Sub Directories - Using File IO Count Number of Entries in the file    
  */
 
 import java.util.ArrayList;
@@ -45,7 +53,7 @@ public class EmployeePayrollService {
 		employeePayrollList.add(new EmployeePayrollData(id, name, salary));
 	}
 
-	private void writeEmployeePayrollData() {
+	public void writeEmployeePayrollData() {
 		System.out.println("\nWriting Employee Payroll to console\n" + employeePayrollList);
 	}
 
